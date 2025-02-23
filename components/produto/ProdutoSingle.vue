706dfa6360f1f8350b9ab7c78bc2db391b18fae6
João J <etiontdn@gmail.com>
2025-02-23 13:31:30 -0300

    adicionado mais estilos base

--- components/produto/ProdutoSingle.vue
+++ components/produto/ProdutoSingle.vue
@@ -1,3 +1,12 @@
 <template>
-    <div></div>
+    <div class="produto-single">
+        <figure></figure>
+        <div class="produto-info">
+            <p class="produto-nome"></p>
+            <p class="produto-descricao"></p>
+            <p class="produto-preco"></p>
+        </div>
+    </div>
 </template>
+<style lang="less">
+</style>
